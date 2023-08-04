1911f6f1376f33c292e7336a36d8a6d851df7a25
avantikakende <140887896+avantikakende@users.noreply.github.com>
2023-08-04 16:29:55 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+  int a,b,s;
+  scanf("%d",&a);
+  scanf("%d",&b);
+  s=a+b;
+  printf("%d",s);
+  return 0;
+}
+*/
+
+
+#include <stdio.h>
+{
+int a,b,c
+scanf("%d %d",&b,&b);
+c=a-b;
+  printf("%d",c);
+  return 0;
+}
+
+
+
